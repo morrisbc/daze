636d670f409360dfb987df48c733208aa8e57b7d
morrisbc <31737772+morrisbc@users.noreply.github.com>
2019-10-11 00:06:27 -0400

    Added average and goal value marker lines to BarGraph component

--- src/components/BarGraph.js
+++ src/components/BarGraph.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Bar from "./Bar";
 
-const BarGraph = ({ labels, values, unit, barColor }) => {
+const BarGraph = ({ labels, values, unit, goalValue, barColor }) => {
   // Throw Error if the number of labels doesn't match the number of values
   if (labels.length !== values.length) {
     throw new Error();
@@ -16,6 +16,9 @@ const BarGraph = ({ labels, values, unit, barColor }) => {
     scale.push(num);
   }
 
+  // Courtesy of https://codeburst.io/javascript-arrays-finding-the-minimum-maximum-sum-average-values-f02f1b0ce332
+  const avgValue = values.reduce((a, b) => a + b, 0) / values.length;
+
   return (
     <div
       className="d-flex flex-column"
@@ -26,19 +29,34 @@ const BarGraph = ({ labels, values, unit, barColor }) => {
       }}
     >
       <div
-        className="d-flex justify-content-around text-white pt-1"
+        className="d-flex justify-content-around text-white py-1"
         style={{ borderBottom: "1px solid white" }}
       >
         {labels.map(label => (
-          <label key={label} htmlFor={`${label}-bar`}>
+          <label className="m-0" key={label} htmlFor={`${label}-bar`}>
             {label}
           </label>
         ))}
       </div>
       <div className="position-relative h-100 d-flex justify-content-around align-items-end px-1">
+        {goalValue && (
+          <hr
+            className="w-100 position-absolute border border-primary m-0"
+            style={{
+              bottom: `${(goalValue / (maxValue + 1)) * 100}%`
+            }}
+          />
+        )}
+        <hr
+          className="w-100 position-absolute m-0"
+          style={{
+            bottom: `${(avgValue / (maxValue + 1)) * 100}%`,
+            border: "1px solid gold"
+          }}
+        />
         <div
-          className="position-absolute h-100 d-flex flex-column justify-content-between align-items-end text-white pr-3"
-          style={{ left: "-50px" }}
+          className="position-absolute d-flex flex-column justify-content-between align-items-end text-white pr-3"
+          style={{ left: "-50px", bottom: "-7px", height: "109%" }}
         >
           {scale.map(value => (
             <p key={value} className="m-0">
@@ -64,6 +82,7 @@ BarGraph.propTypes = {
   labels: PropTypes.array.isRequired,
   values: PropTypes.array.isRequired,
   unit: PropTypes.string.isRequired,
+  goalValue: PropTypes.number,
   barColor: PropTypes.string
 };
 
--- src/components/pages/dashboard/Sleep.js
+++ src/components/pages/dashboard/Sleep.js
@@ -5,18 +5,30 @@ import BarGraph from "../../BarGraph";
 const Sleep = ({ sleepGoal, sleepAvg }) => {
   const labels = ["Su", "M", "T", "W", "Th", "F", "S"];
   const values = [6, 7, 6, 8, 8, 7, 6.5];
+  // Courtesy of https://codeburst.io/javascript-arrays-finding-the-minimum-maximum-sum-average-values-f02f1b0ce332
+  const avgValue = values.reduce((a, b) => a + b, 0) / values.length;
 
   return (
     <div className="h-100 d-flex flex-column">
       <div className="d-flex justify-content-around text-white my-4">
-        <h3>
+        <h3
+          className="border-primary pb-1"
+          style={{ borderBottom: "2px solid" }}
+        >
           <i className="fas fa-trophy"></i> Goal: {sleepGoal} hours
         </h3>
-        <h3>
-          <i className="fas fa-history"></i> Average: {sleepAvg} hours
+        <h3 className="pb-1" style={{ borderBottom: "2px solid gold" }}>
+          <i className="fas fa-history"></i> Average: {avgValue.toFixed(2)}{" "}
+          hours
         </h3>
       </div>
-      <BarGraph labels={labels} values={values} unit={"h"} barColor="#3f808b" />
+      <BarGraph
+        labels={labels}
+        values={values}
+        unit={"h"}
+        goalValue={sleepGoal}
+        barColor="#3f808b"
+      />
     </div>
   );
 };
